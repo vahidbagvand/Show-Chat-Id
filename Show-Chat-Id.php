6f741ba3668cb68bfb1786402a3df37ef5f3d22f
vahidbagvand <103110394+vahidbagvand@users.noreply.github.com>
2023-02-11 14:27:49 +0330

    Update Show-Chat-Id.php

--- Show-Chat-Id.php
+++ Show-Chat-Id.php
@@ -1,26 +1,38 @@
-<?php
-include 'Telegram.php';  
-//// Use the Telegram library in addition to your files
-/////  https://github.com/Eleirbag89/TelegramBotPHP
-$bot_token = '___TOKEN___';
-$telegram = new Telegram($bot_token);
-$text       = $telegram->Text();
-$username   = $telegram->Username();
-$name       = $telegram->FirstName();
-$family     = $telegram->LastName();
-$message_id = $telegram->MessageID();
-$user_id    = $telegram->UserID();
-$chat_id    = $telegram->ChatID(); 
-$msgType    = $telegram->getUpdateType();
-if($text == '/start'){
-    $content = array('chat_id' => $chat_id, 'text' => $message_id, 'text' =>
-    "🟢 Hello ".$name."\n\n🟣 CHAT ID : ".$chat_id."\n\n🔵 Username : @".$username);
-    $telegram->sendMessage($content);
-    die();
-}else {
-    $content = array('chat_id' => $chat_id, 'text' => $message_id, 'text' =>
-    "🟢 Hello ".$name."\n\n🟣 CHAT ID : ".$chat_id."\n\n🔵 Username : @".$username);
-    $telegram->sendMessage($content);
-    die();
+<?php 
+
+ob_start();
+//توکن ربات خود را زیر وارد کنید
+$API_KEY = 'tokennnnnnnnnnnnnnnnnnnnnnnnnnnnnn';
+##------------------------------##
+define('API_KEY',$API_KEY);
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
 }
+	
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$chat_id = $message->chat->id;
+$text2 = $message ;
+$first_name = $update->message->chat->first_name;
+$username = $update->message->chat->username;
+
+if($text2 != null){
+	bot('sendMessage',[
+	'chat_id'=>$chat_id,
+	'text'=>"🟢 NAME : $first_name \n\n🟣 Chat Id : $chat_id\n\n🔵 Username : @$username   ",
+	]);
+	}
+	
+
+   
 ?>
